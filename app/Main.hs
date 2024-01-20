ac94dfe87290bc1aa5a9f56d91333f363a4303bf
Peter van der Heijden <heijp06@gmail.com>
2024-01-20 19:53:00 +0100

    Output MD5 if string is passed as argument.

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,16 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import Data.Hash.MD5 (Str(Str), md5s)
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  args <- getArgs
+  case args of
+    [code] -> md5 code
+    _ -> return ()
+
+md5 :: String -> IO ()
+md5 xs = print hash
+  where
+    hash = md5s $ Str xs
