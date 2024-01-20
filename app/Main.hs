dc5062a6ffcd1d373697e75219b9931acdcf3b93
Peter van der Heijden <heijp06@gmail.com>
2024-01-20 20:26:29 +0100

    Read integers from file.

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,27 @@
 module Main where
 
+import qualified Data.ByteString as B
 import Data.Hash.MD5 (Str(Str), md5s)
+import Data.List.Split (chunksOf)
 import System.Environment (getArgs)
+import System.IO (openBinaryFile, IOMode (ReadMode))
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
     [code] -> md5 code
-    _ -> return ()
+    _ -> do
+      ps <- program
+      print ps
 
 md5 :: String -> IO ()
 md5 xs = print hash
   where
     hash = md5s $ Str xs
+
+program :: IO [Int]
+program = do
+  h <- openBinaryFile "challenge.bin" ReadMode
+  inputBytes <- map fromIntegral . B.unpack <$> B.hGetContents h
+  return . map (\ xs -> head xs + 256 * xs !! 1) $ chunksOf 2 inputBytes
