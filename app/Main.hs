32f8c80d696166387fa54087d7cbfbfb124e7d2e
Peter van der Heijden <heijp06@gmail.com>
2024-01-21 10:55:20 +0100

    Start implementing processor logic.

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,33 @@
 module Main where
 
+import Control.Monad (unless)
 import qualified Data.ByteString as B
 import Data.Hash.MD5 (Str(Str), md5s)
 import Data.List.Split (chunksOf)
 import System.Environment (getArgs)
 import System.IO (openBinaryFile, IOMode (ReadMode))
+import Processor (Processor(..), processor, run)
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [code] -> md5 code
+    [code] -> print . md5s $ Str code
     _ -> do
       ps <- program
-      print ps
-
-md5 :: String -> IO ()
-md5 xs = print hash
-  where
-    hash = md5s $ Str xs
+      loop $ processor ps
 
 program :: IO [Int]
 program = do
   h <- openBinaryFile "challenge.bin" ReadMode
   inputBytes <- map fromIntegral . B.unpack <$> B.hGetContents h
   return . map (\ xs -> head xs + 256 * xs !! 1) $ chunksOf 2 inputBytes
+
+loop :: Processor -> IO ()
+loop proc = do
+  let proc' = run proc
+  let xs = output proc'
+  unless (null xs) $ putStrLn xs
+  if halted proc'
+    then putStrLn $ err proc'
+    else loop proc'
---
+++ src/Processor.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Processor
+    ( Processor(halted, err, output)
+    , processor
+    , run
+    ) where
+
+import Control.Monad.State (State, get, put, execState, unless)
+import Data.Array ((!), Array)
+import Data.Char (chr)
+import GHC.Arr (listArray)
+import Prelude hiding (read)
+
+data Processor = Processor { memory :: Array Int Int
+                           , halted :: Bool
+                           , instructionPointer :: Int
+                           , output :: String
+                           , err :: String
+                           }
+
+type ProcessorState = State Processor
+
+processor :: [Int] -> Processor
+processor code = Processor mem False 0 "" ""
+    where
+        mem = listArray (0, 32767) $ code ++ replicate (32768 - length code) 0
+
+run :: Processor -> Processor
+run = execState doRun
+
+doRun :: ProcessorState ()
+doRun = do
+    ins <- read
+    execute ins
+    Processor{..} <- get
+    unless halted doRun
+
+read :: ProcessorState Int
+read = do
+    Processor{..} <- get
+    let value = memory ! instructionPointer
+    put Processor { instructionPointer = instructionPointer + 1, .. }
+    return value
+
+execute :: Int -> ProcessorState ()
+execute 0 = halt
+execute 19 = out
+execute 21 = noop
+execute ins = raise $ "Instruction not implemented: " ++ show ins ++ "."
+
+halt :: ProcessorState ()
+halt = raise "Process halted."
+
+out :: ProcessorState ()
+out = do
+    i <- read
+    Processor{..} <- get
+    put Processor { output = output ++ [chr i], .. }
+
+noop :: ProcessorState ()
+noop = return ()
+
+raise :: String -> ProcessorState ()
+raise msg = do
+    proc <- get
+    put $ proc { err = msg, halted = True }
