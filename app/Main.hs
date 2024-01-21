cbd61111a813ea4882553590cc921424876a1169
Peter van der Heijden <heijp06@gmail.com>
2024-01-21 12:32:31 +0100

    Add colors.

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ import Control.Monad (unless)
 import qualified Data.ByteString as B
 import Data.Hash.MD5 (Str(Str), md5s)
 import Data.List.Split (chunksOf)
+import System.Console.ANSI (setSGR, SGR (SetColor, Reset), ConsoleLayer (Foreground), ColorIntensity (Vivid), Color (Yellow, Red))
 import System.Environment (getArgs)
 import System.IO (openBinaryFile, IOMode (ReadMode))
 import Processor (Processor(..), processor, run)
@@ -27,7 +28,12 @@ loop :: Processor -> IO ()
 loop proc = do
   let proc' = run proc
   let xs = output proc'
+  setSGR [SetColor Foreground Vivid Yellow]
   unless (null xs) $ putStrLn xs
+  setSGR [Reset]
   if halted proc'
-    then putStrLn $ err proc'
+    then do
+      setSGR [SetColor Foreground Vivid Red]
+      putStrLn $ err proc'
+      setSGR [Reset]
     else loop proc'
